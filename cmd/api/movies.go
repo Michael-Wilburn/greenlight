50478c28e7ae774e06cbf7927903761b92d499b6
Michael Wilburn <michael-mw@hotmail.com>
2024-07-17 23:20:18 -0300

    Fetching a Movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,92 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/Michael-Wilburn/greenlight/internal/data"
 	"github.com/Michael-Wilburn/greenlight/internal/validator"
 )
 
-// func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-// 	/* Declare an anonymous struct to hold the information that we expect to be in the
-// 	   HTTP request body (note that the field names and types in the struct are a subset
-// 	   of the Movie struct that we created earlier). This struct will be our *target
-// 	   decode destination*.
-// 	*/
-// 	var input struct {
-// 		Title   string       `json:"title"`
-// 		Year    int32        `json:"year"`
-// 		Runtime data.Runtime `json:"runtime"`
-// 		Genres  []string     `json:"genres"`
-// 	}
-
-// 	/* Use the new readJSON() helper to decode the request body into the input struct.
-// 	If this returns an error we send the client the error message along with a 400
-// 	Bad Request status code, just like before.
-// 	*/
-// 	err := app.readJSON(w, r, &input)
-// 	if err != nil {
-// 		// Use the new badRequestResponse() helper.
-// 		app.badRequestResponse(w, r, err)
-// 		return
-// 	}
-
-// 	// Note that the movie variable contains a *pointer* to a Movie struct.
-// 	movie := &data.Movie{
-// 		Title:   input.Title,
-// 		Year:    input.Year,
-// 		Runtime: input.Runtime,
-// 		Genres:  input.Genres,
-// 	}
-
-// 	// Initialize a new Validator.
-// 	v := validator.New()
-
-// 	// Call the ValidateMovie() function and return a response containing the errors if
-// 	// any of the checks fail.
-// 	if data.ValidateMovie(v, movie); !v.Valid() {
-// 		app.failedValidationResponse(w, r, v.Errors)
-// 		return
-// 	}
-
-// 	/* Call the Insert() method on our movies model, passing in a pointer to the
-// 	   validated movie struct. This will create a record in the database and update the
-// 	   movie struct with the system-generated information.*/
-// 	err = app.models.Movies.Insert(movie)
-// 	if err != nil {
-// 		app.serverErrorResponse(w, r, err)
-// 		return
-// 	}
-
-// 	/* When sending a HTTP response, we want to include a Location header to let the
-// 	   client know which URL they can find the newly-created resource at. We make an
-// 	   empty http.Header map and then use the Set() method to add a new Location header,
-// 	   interpolating the system-generated ID for our new movie in the URL.*/
-// 	headers := make(http.Header)
-// 	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
-
-// 	// Write a JSON response with a 201 Created status code, the movie data in the
-// 	// response body, and the Location header.
-// 	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
-// 	if err != nil {
-// 		app.serverErrorResponse(w, r, err)
-// 	}
-// }
-
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	/* Declare an anonymous struct to hold the information that we expect to be in the
+	   HTTP request body (note that the field names and types in the struct are a subset
+	   of the Movie struct that we created earlier). This struct will be our *target
+	   decode destination*.
+	*/
 	var input struct {
 		Title   string       `json:"title"`
 		Year    int32        `json:"year"`
 		Runtime data.Runtime `json:"runtime"`
 		Genres  []string     `json:"genres"`
 	}
+
+	/* Use the new readJSON() helper to decode the request body into the input struct.
+	If this returns an error we send the client the error message along with a 400
+	Bad Request status code, just like before.
+	*/
 	err := app.readJSON(w, r, &input)
 	if err != nil {
+		// Use the new badRequestResponse() helper.
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
 	// Note that the movie variable contains a *pointer* to a Movie struct.
 	movie := &data.Movie{
 		Title:   input.Title,
@@ -94,25 +40,33 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		Runtime: input.Runtime,
 		Genres:  input.Genres,
 	}
+
+	// Initialize a new Validator.
 	v := validator.New()
+
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
 	if data.ValidateMovie(v, movie); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
-	// Call the Insert() method on our movies model, passing in a pointer to the
-	// validated movie struct. This will create a record in the database and update the
-	// movie struct with the system-generated information.
+
+	/* Call the Insert() method on our movies model, passing in a pointer to the
+	   validated movie struct. This will create a record in the database and update the
+	   movie struct with the system-generated information.*/
 	err = app.models.Movies.Insert(movie)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
-	// When sending a HTTP response, we want to include a Location header to let the
-	// client know which URL they can find the newly-created resource at. We make an
-	// empty http.Header map and then use the Set() method to add a new Location header,
-	// interpolating the system-generated ID for our new movie in the URL.
+
+	/* When sending a HTTP response, we want to include a Location header to let the
+	   client know which URL they can find the newly-created resource at. We make an
+	   empty http.Header map and then use the Set() method to add a new Location header,
+	   interpolating the system-generated ID for our new movie in the URL.*/
 	headers := make(http.Header)
 	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
 	// Write a JSON response with a 201 Created status code, the movie data in the
 	// response body, and the Location header.
 	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
@@ -138,19 +92,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	/*
-		Create a new instance of the Movie struct, containing the ID we extracted from
-		the URL and some dummy data. Also notice that we deliberately haven't set a
-		value for the Year field.
+	/* Call the Get() method to fetch the data for a specific movie. We also need to
+	   use the errors.Is() function to check if it returns a data.ErrRecordNotFound
+	   error, in which case we send a 404 Not Found response to the client.
 	*/
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Year:      1942,
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	// Encode the struct to JSON and sent it as the HTTP response.
