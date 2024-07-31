6e3b62d5143032882f47dc9147e83a123c2eb5b6
Michael Wilburn <michael-mw@hotmail.com>
2024-07-31 09:25:25 -0300

    Structured JSON Log Entries

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -11,7 +11,12 @@ upgrade this to use structed logging, and record additional information
 about the request including the HTTP method and URL.
 */
 func (app *application) logError(r *http.Request, err error) {
-	app.logger.Println(err)
+	// Use the PrintError() method to log the error message, and include the current
+	// request method and URL as properties in the log entry.
+	app.logger.PrintError(err, map[string]string{
+		"request_method": r.Method,
+		"request_url":    r.URL.String(),
+	})
 }
 
 /*
@@ -72,7 +77,7 @@ func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
 
-func(app *application) editConflictResponse(w http.ResponseWriter, r *http.Request){
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
 	message := "unable to update the record due to an edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
