212398a3386b4534875e23d24f5abd4dbefdbb35
unknown <michael-mw@hotmail.com>
2024-12-16 08:09:18 -0300

    Fixed-Format JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,7 +8,18 @@ import (
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a fixed-format JSON response from a string. Notice how we're using a raw
+	// string literal(enclose with backticks) so that we can include double-quote
+	// characters in the JSON withput needing to escape them? We also use the %q verb to
+	// wrap the interpolated values in double-quotes.
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// Set the Content-Type header to application/json so that the client knows to
+	// expect JSON data. Go will deffault to sending a
+	//"Content-type: text/plain; charset=utf8" header instead
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON as the HTTP response body.
+	w.Write([]byte(js))
 }
